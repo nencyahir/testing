8eaa7a2a53f07911c763489f50e41327017e7876
Nency-Bacancy <nency.vaghamshi@bacancy.com>
2025-12-05 10:56:35 +0530

    Removed Everythig

--- app/models/employee.rb
+++
@@ -1,3 +0,0 @@
-class Employee < ApplicationRecord
-    validates :full_name, :job_title, :country, :salary, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_04_150058) do
-  create_table "employees", force: :cascade do |t|
-    t.string "full_name"
-    t.string "job_title"
-    t.string "country"
-    t.integer "salary"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[8.0].define(version: 0) do
 end
