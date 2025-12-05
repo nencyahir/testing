3ecf3ba494a30ee96a7f289621e36ac2def8922d
Nency-Bacancy <nency.vaghamshi@bacancy.com>
2025-12-05 14:58:05 +0530

    Rubocop changes

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::EmployeesController < ApplicationController
     def create
         employee = Employee.new(employee_params)
-    
+
         if employee.save
           render json: employee, status: :created
         else
           render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
         end
       end
-    
+
       private
-    
+
       def employee_params
         params.require(:employee).permit(:full_name, :job_title, :country, :salary)
       end
